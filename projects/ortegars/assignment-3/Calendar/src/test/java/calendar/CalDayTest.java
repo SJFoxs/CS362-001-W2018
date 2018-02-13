801f6bc78be9bc8d9c7e46b94f9dbce377d8087a
Sergio Ortega <sjfox94@gmail.com>
2018-02-12 23:25:17 -0800

    Final tests
    Cleaned errors and updated test suites to the final state.

--- projects/ortegars/assignment-3/Calendar/src/test/java/calendar/CalDayTest.java
+++ projects/ortegars/assignment-3/Calendar/src/test/java/calendar/CalDayTest.java
@@ -44,7 +44,69 @@ public class CalDayTest {
 	 }
 	 @Test
 	  public void test02()  throws Throwable  {
-		 
+		  int startHour=14;
+		  int startMinute=55;
+		  int startDay=24;
+		  int startMonth=2;
+		  int startYear=2018;
+		  String title="Test Appointment";
+		  String description="A valid appointment";
+		  GregorianCalendar date = new GregorianCalendar(2018, 2, 24);
+		  		  
+		  Appt appt = new Appt(startHour, 
+		    startMinute, 
+			startDay, 
+			startMonth, 
+			startYear,
+			title,
+			description);
+			
+		  Appt appt2 = new Appt(startHour, 
+		    startMinute, 
+			startDay, 
+			startMonth, 
+			startYear,
+			title,
+			description);
+			
+		  CalDay day = new CalDay(date);
+		  assertNotNull(day.getAppts());
+		  day.addAppt(appt);
+		  assertNotNull(day.toString());
 	 }
+	 @Test
+	  public void test03() throws Throwable  {
+		  Appt validAppt = new Appt(12, 35, 23, 1, 2018, "Counter appt", "First test");
+		  Appt invalidAppt = new Appt(11, 71, 23, 1, 2018, "Invalid appt", "Second test");
+		  Appt vAppt = new Appt(10, 35, 23, 1, 2018, "Counter appt", "Third test");
+		  Appt ivAppt = new Appt(24, 71, 23, 1, 2018, "Invalid appt", "Fourth test");
+		  Appt vaAppt = new Appt(10, 45, 23, 1, 2018, "Counter appt", "Fifth test");
+		  
+		  GregorianCalendar date = new GregorianCalendar(2018, 1, 23);
+		  
+		  CalDay today = new CalDay(date);
+		  CalDay ivDay = new CalDay();
+		  //today.addAppt(invalidAppt);
+		  CalDay testDay = new CalDay(date);
+		  
+		  
+		  today.addAppt(validAppt);
+		  today.addAppt(invalidAppt);
+		  today.addAppt(vAppt);
+		  
+		  testDay.addAppt(vAppt);
+		  testDay.addAppt(ivAppt);
+		  testDay.addAppt(validAppt);
+		  testDay.addAppt(invalidAppt);
+		  testDay.addAppt(vAppt);
+		  testDay.addAppt(vaAppt);
+		  
+		  assertEquals(testDay.getAppts().get(0), vAppt);
+		  assertEquals(testDay.getAppts().size(), 4);
+		  assertEquals(testDay.getAppts().get(2), vaAppt);
+		  assertNotNull(testDay.iterator());
+		  assertNull(ivDay.iterator());
+		  assertNotNull(testDay.getAppts());
+	  }
 //add more unit tests as you needed	
 }
--- projects/ortegars/assignment-3/Calendar/src/test/java/calendar/TimeTableTest.java
+++ projects/ortegars/assignment-3/Calendar/src/test/java/calendar/TimeTableTest.java
@@ -19,9 +19,6 @@ public class TimeTableTest {
 	  public void test01()  throws Throwable  {
 		TimeTable tt = new TimeTable();
 		LinkedList<Appt> unsortedAppts = new LinkedList<Appt>();
-		LinkedList<CalDay> apptRange = new LinkedList<CalDay>();
-		GregorianCalendar today = new GregorianCalendar(2018, 1, 23);
-		GregorianCalendar tomorrow = new GregorianCalendar(2018, 1, 24);
 		
 		Appt apptOne = new Appt(12, 35, 23, 1, 2018, "Counter appt", "First test");
 		Appt apptTwo = new Appt(15, 35, 23, 1, 2018, "Counter appt", "Second test");
@@ -35,6 +32,7 @@ public class TimeTableTest {
 		unsortedAppts.add(apptFour);
 		
 		unsortedAppts = tt.deleteAppt(unsortedAppts, null);
+		assertNull(unsortedAppts);
 		
 		unsortedAppts = new LinkedList<Appt>();
 		unsortedAppts.add(apptOne);
@@ -42,6 +40,7 @@ public class TimeTableTest {
 		unsortedAppts.add(apptThree);
 		unsortedAppts.add(apptFour);
 		unsortedAppts = tt.deleteAppt(null, apptOne);
+		assertNull(unsortedAppts);
 		
 		unsortedAppts = new LinkedList<Appt>();
 		unsortedAppts.add(apptOne);
@@ -50,6 +49,7 @@ public class TimeTableTest {
 		unsortedAppts.add(apptFour);
 		
 		unsortedAppts = tt.deleteAppt(null, null);
+		assertNull(unsortedAppts);
 		
 		unsortedAppts = new LinkedList<Appt>();
 		unsortedAppts.add(apptOne);
@@ -60,6 +60,7 @@ public class TimeTableTest {
 		unsortedAppts = tt.deleteAppt(unsortedAppts, apptTwo);
 		
 		unsortedAppts = tt.deleteAppt(unsortedAppts, apptTwo);
+		assertNull(unsortedAppts);
 		
 		unsortedAppts = new LinkedList<Appt>();
 		unsortedAppts.add(apptOne);
@@ -68,19 +69,45 @@ public class TimeTableTest {
 		unsortedAppts.add(apptFour);
 		
 		unsortedAppts = tt.deleteAppt(unsortedAppts, apptFive);
+		assertNull(unsortedAppts);
 		
+		unsortedAppts = new LinkedList<Appt>();
+		unsortedAppts.add(apptOne);
+		unsortedAppts.add(apptTwo);
+		unsortedAppts.add(apptThree);
+		unsortedAppts.add(apptFour);
 		
+		unsortedAppts = tt.deleteAppt(unsortedAppts, apptTwo);
+		assertNotNull(unsortedAppts);
 		
+		unsortedAppts = new LinkedList<Appt>();
+		unsortedAppts.add(apptOne);
+		unsortedAppts.add(apptTwo);
+		unsortedAppts.add(apptThree);
+		unsortedAppts.add(apptFour);
 		
-		//apptRange = tt.getApptRange(unsortedAppts, tomorrow, today);
+		assertNull(tt.deleteAppt(unsortedAppts, apptFour));
+		
+		unsortedAppts = new LinkedList<Appt>();
+		unsortedAppts.add(apptOne);
+		unsortedAppts.add(apptTwo);
+		unsortedAppts.add(apptThree);
+		unsortedAppts.add(apptFour);
+		LinkedList<Appt> copyAppt = new LinkedList<Appt>();
+		copyAppt.add(apptOne);
+		copyAppt.add(apptTwo);
+		copyAppt.add(apptFour);
+		
+		assertEquals(tt.deleteAppt(unsortedAppts, apptThree), copyAppt);
 	 }
 	 @Test
 	  public void test02()  throws Throwable  {
 		TimeTable tt = new TimeTable();
 		LinkedList<Appt> unsortedAppts = new LinkedList<Appt>();
 		LinkedList<CalDay> apptRange = new LinkedList<CalDay>();
-		GregorianCalendar today = new GregorianCalendar(2018, 1, 23);
-		GregorianCalendar tomorrow = new GregorianCalendar(2018, 1, 24);
+		LinkedList<Appt> testRange = new LinkedList<Appt>();
+		GregorianCalendar firstDay = new GregorianCalendar(2018, 1, 1);
+		GregorianCalendar lastDay = new GregorianCalendar(2018, 1, 28);
 		
 		Appt apptOne = new Appt(12, 35, 23, 1, 2018, "Counter appt", "First test");
 		Appt apptTwo = new Appt(15, 35, 23, 1, 2018, "Counter appt", "Second test");
@@ -88,51 +115,51 @@ public class TimeTableTest {
 		Appt apptFour = new Appt(12, 35, 24, 1, 2018, "Counter appt", "Fourth test");
 		Appt apptFive = new Appt(12, 71, 23, 1, 2018, "Invalid appt", "Fifth test");
 
-		
+		assertNotNull(tt.getApptRange(unsortedAppts, firstDay, lastDay));
 		unsortedAppts.add(apptOne);
-		unsortedAppts.add(apptTwo);
 		unsortedAppts.add(apptThree);
 		unsortedAppts.add(apptFour);
-		apptRange = tt.getApptRange(unsortedAppts, today, tomorrow);
-		//apptRange = tt.getApptRange(unsortedAppts, today, today);
-		
-		int []arr={1,0,2,3};
-		unsortedAppts = tt.permute(unsortedAppts, arr);
-		
-		unsortedAppts.add(apptFive);
-		unsortedAppts.add(apptOne);
 		unsortedAppts.add(apptTwo);
-		unsortedAppts.add(apptThree);
-		unsortedAppts.add(apptFour);
-		apptRange = tt.getApptRange(unsortedAppts, today, tomorrow);
-		
-		int []invArr={1,2,0};
-		//unsortedAppts = tt.permute(unsortedAppts, invArr);
+		testRange.add(apptOne);
+		testRange.add(apptTwo);
+		testRange.add(apptThree);
+		testRange.add(apptFour);
+		int []pv = {3, 2, 2, 3};
+		assertNotNull(tt.permute(unsortedAppts, pv));
 	 }
 	 @Test
 	  public void test03() throws Throwable{
 		TimeTable tt = new TimeTable();
-		LinkedList<Appt> unsortedAppts = new LinkedList<Appt>();
-		LinkedList<CalDay> apptRange = new LinkedList<CalDay>();
-		GregorianCalendar today = new GregorianCalendar(2018, 1, 23);
-		GregorianCalendar tomorrow = new GregorianCalendar(2018, 1, 24);
+		LinkedList<Appt> sortedAppts = new LinkedList<Appt>();
+		//LinkedList<CalDay> apptRange = new LinkedList<CalDay>();
+		LinkedList<CalDay> testRange = new LinkedList<CalDay>();
+		GregorianCalendar oneDay = new GregorianCalendar(2018, 2, 1);
+		GregorianCalendar twoDay = new GregorianCalendar(2018, 2, 28);
+		GregorianCalendar day = new GregorianCalendar(2018, 2, 2);
 		
-		Appt apptOne = new Appt(12, 35, 23, 1, 2018, "Counter appt", "First test");
-		Appt apptTwo = new Appt(15, 35, 23, 1, 2018, "Counter appt", "Second test");
-		Appt apptThree = new Appt(10, 35, 23, 1, 2018, "Counter appt", "Third test");
-		Appt apptFour = new Appt(12, 35, 24, 1, 2018, "Counter appt", "Fourth test");
-		Appt apptFive = new Appt(12, 71, 23, 1, 2018, "Invalid appt", "Fifth test");
+		Appt apptOne = new Appt(12, 35, 1, 2, 2018, "Counter appt", "First test");
+		Appt apptTwo = new Appt(15, 35, 2, 2, 2018, "Counter appt", "Second test");
+		Appt apptThree = new Appt(10, 35, 3, 2, 2018, "Counter appt", "Third test");
+		Appt apptFour = new Appt(12, 35, 5, 2, 2018, "Counter appt", "Fourth test");
+		Appt apptFive = new Appt(12, 71, 4, 2, 2018, "Invalid appt", "Fifth test");
 		
-		int []recurDays={2,3,4};
+		int []recurDays={4};
 				
-		apptOne.setRecurrence(recurDays, Appt.RECUR_BY_WEEKLY, 2, Appt.RECUR_NUMBER_FOREVER);
+		apptOne.setRecurrence(recurDays, Appt.RECUR_BY_WEEKLY, 1, 1);
 		
-		unsortedAppts.add(apptFive);
-		unsortedAppts.add(apptOne);
-		unsortedAppts.add(apptTwo);
-		unsortedAppts.add(apptThree);
-		unsortedAppts.add(apptFour);
-		apptRange = tt.getApptRange(unsortedAppts, today, tomorrow);
+		assertEquals((tt.getApptRange(sortedAppts, oneDay, day)).size(), 1);
+		
+		sortedAppts.add(apptOne);
+		sortedAppts.add(apptTwo);
+		sortedAppts.add(apptThree);
+		sortedAppts.add(apptFour);
+		sortedAppts.add(apptFive);
+		
+		testRange = tt.getApptRange(sortedAppts, oneDay, twoDay);
+		assertEquals((testRange.get(2)).getAppts().get(0), apptTwo);
+		assertNotNull((testRange.get(2)).getAppts().get(0));
+		assertEquals((testRange.get(0)).getAppts().get(0), apptOne);
+		assertEquals((testRange.get(0)).getAppts().get(0), (testRange.get(12)).getAppts().get(0));
 	 }
 //add more unit tests as you needed
 }
